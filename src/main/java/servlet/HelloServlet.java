bcb37b50d809cee28c8d02da5c73d12275a28e34
Chris <jeiofj@gmail.com>
2020-03-31 15:34:52 -0400

    Update HelloServlet.java

--- src/main/java/servlet/HelloServlet.java
+++ src/main/java/servlet/HelloServlet.java
@@ -311,7 +311,7 @@ outline: 0.25rem solid lightblue;
             <link rel='stylesheet' href='https://use.fontawesome.com/releases/v5.5.0/css/all.css'
                 integrity='sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU' crossorigin='anonymous'>
                 <script src='https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js'></script>
-            
+                <link rel=\'stylesheet\' type=\'text/css\' href=\'' + Style + '\'>
         </head>
         
         <body>
